d89c7bf7ed4644416b7c93038af4688029a124a8
baevpavel <123824756+baevpavel@users.noreply.github.com>
2023-04-06 23:41:55 +0500

    Update Work_Postgres_Python.py

--- Work_Postgres_Python.py
+++ Work_Postgres_Python.py
@@ -3,20 +3,16 @@ import psycopg2
 def create_db(conn):
     with conn.cursor() as cur:
         cur.execute("""
-        
         CREATE TABLE IF NOT EXISTS Clients (
             client_id SERIAL PRIMARY KEY,
             first_name VARCHAR(50) NOT NULL,
             last_name VARCHAR(50) NOT NULL,
-            email VARCHAR(30) NOT NULL
+            email VARCHAR(30) NOT NULL UNIQUE
         );	
         CREATE TABLE IF NOT EXISTS Phones (
             id INTEGER NOT NULL REFERENCES Clients ON DELETE CASCADE,
-            phone INTEGER NOT NULL UNIQUE,
-            #CONSTRAINT ch PRIMARY KEY (id, phone)
+            phone VARCHAR(11) NOT NULL UNIQUE
         );
-#    conn.commit()  # фиксируем в БД
-#conn.close()        
                 """)
 
 def add_client(conn, first_name, last_name, email, phone=None):
@@ -38,9 +34,12 @@ def find_client(conn, first_name=None, last_name=None, email=None, phone=None):
     pass
 
 def drop_table()
-    DROP TABLE IF NOT EXISTS Phones ON DELETE CASCADE;
-    DROP TABLE IF NOT EXISTS Clients;
+    with conn.cursor() as cur:
+        cur.execute("""
+        DROP TABLE IF EXISTS Phones CASCADE;
+        DROP TABLE IF EXISTS Clients;
+                """)
 
-with psycopg2.connect(database="clients_db", user="postgres", password="postgres") as conn:
+with psycopg2.connect(database="client_db", user="postgres", password="postgres") as conn:
     drop_table(conn)# удаляем таблицы
     create_db(conn)  # вызывайте функции здесь
