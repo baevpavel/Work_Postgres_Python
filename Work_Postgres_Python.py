7972ea0599a3a9d56bf874718a9610ad54e1e53e
baevpavel <123824756+baevpavel@users.noreply.github.com>
2023-04-12 22:53:18 +0500

    Update Work_Postgres_Python.py
    refactoring

--- Work_Postgres_Python.py
+++ Work_Postgres_Python.py
@@ -1,5 +1,12 @@
 import psycopg2
 
+def drop_db(conn):
+    with conn.cursor() as cur:
+        cur.execute("""
+        DROP TABLE IF EXISTS Phones CASCADE;
+        DROP TABLE IF EXISTS Clients;
+            """)
+
 def create_db(conn):
     with conn.cursor() as cur:
         cur.execute("""
@@ -8,44 +15,47 @@ def create_db(conn):
             first_name VARCHAR(50) NOT NULL,
             last_name VARCHAR(50) NOT NULL,
             email VARCHAR(30) NOT NULL UNIQUE
-        );	
+        );
         CREATE TABLE IF NOT EXISTS Phones (
             id INTEGER NOT NULL REFERENCES Clients ON DELETE CASCADE,
-            phone VARCHAR(11) NOT NULL UNIQUE
+            phone VARCHAR(11) UNIQUE
         );
                 """)
 
 def add_client(conn, first_name, last_name, email, phone=None):
     with conn.cursor() as cur:
+        if find_client(conn, first_name, last_name, email):
+            return "Client not unique!"
+        if phone != None:
+            if find_client(conn, phone=phone):
+                return "Phone not unique!"
         cur.execute("""
         INSERT INTO Clients VALUES
         (DEFAULT, %s, %s, %s) RETURNING client_id;
         """, (first_name, last_name, email))
-
         if phone != None:
             add_phone(conn, cur.fetchone(), phone)
 
         return (first_name, last_name, email, phone)
 
 def add_phone(conn, client_id, phone):
+    if find_client(conn, phone=phone):
+        return "Phone not unique"
     with conn.cursor() as cur:
         cur.execute("""
-        SELECT client_id FROM %s
-        """, (client_id))
-
+        SELECT client_id FROM Clients
+        WHERE client_id=%s;
+        """,(client_id,))
+        if not cur.fetchone():
+            return "The client does not exist"
         cur.execute("""
         INSERT INTO Phones VALUES
         (%s, %s) RETURNING phone;
-        """, (client_id, phone))
+                """, (client_id, phone))
         return (cur.fetchone()[0])
 
+
 def change_client(conn, client_id, first_name=None, last_name=None, email=None):
-    pass
-
-def delete_phone(conn, client_id, phone):
-    pass
-
-def delete_client(conn, client_id):
     pass
 
 def find_client(conn, first_name=None, last_name=None, email=None, phone=None):
@@ -77,23 +87,20 @@ def find_client(conn, first_name=None, last_name=None, email=None, phone=None):
             list_atr
         )
         return curs.fetchall()
-
-def drop_table()
-    with conn.cursor() as cur:
-        cur.execute("""
-        DROP TABLE IF EXISTS Phones CASCADE;
-        DROP TABLE IF EXISTS Clients;
-        """)
-
 with psycopg2.connect(database="clients_db", user="postgres", password="postgres") as conn:
     pass
     # drop_db(conn)
     # create_db(conn)
-    # add_client(conn,'name1','name2','9@kkt1.ru')
+    print(add_client(conn,'name08','name078','08@kkt1.ru', '89000000002'))
+    # print(find_client(conn,'name05','name05','05@kkt1.ru'))
+    # print(find_client(conn, phone='79242345679'))
     # print("Add client", add_client(conn,'name1','name2','13@kkt.ru', '79125487543'))
-    # print("Add phone", add_phone(conn, 1, '79242345678'))
-    # find_client(conn,'name1')
-    # find_client(conn,'name1', 'name2')
-    # find_client(conn, 'name2', 'name4', 'email2')
-    # find_client(conn, email ='email3')
-    print("Resu", find_client(conn, phone = '79024123453'))
+    # print("Add phone:", add_phone(conn, 1, '89000000003'))
+    # print("Result find: ", find_client(conn,'name1'))
+    # print("Result find: ", find_client(conn,'name1', 'name2'))
+    # print("Result find: ", find_client(conn, email ='6@kkt1.ru'))
+    # print("Result find: ", find_client(conn, phone='71234567890'))
+    # print("Result find: ", find_client(conn, phone='89000000000'))
+
+
+
